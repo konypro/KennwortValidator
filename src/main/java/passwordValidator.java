14e7949a224346d75a4c23aa21f68d2953568268
konypro <ktyker.kt@gmail.com>
2022-10-03 11:09:16 -0700

    add check for digit

--- src/main/java/passwordValidator.java
+++ src/main/java/passwordValidator.java
@@ -2,4 +2,14 @@ public class passwordValidator {
     public static boolean isLongerThan7(String password) {
         return password.length() > 7;
     }
+
+    public static boolean containsDiget(String password) {
+        char [] kagdajabukva = password.toCharArray();
+        for (int i = 0; i<password.length();i++){
+            if (Character.isDigit(kagdajabukva[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -11,5 +11,12 @@ public class ValidatorTest {
     void passwordIsLongEnough(){
         Assertions.assertEquals(true,passwordValidator.isLongerThan7("12345678"));
     }
-
+    @Test
+    void passwordContainDiget(){
+        Assertions.assertEquals(true,passwordValidator.containsDiget("test123"));
+    }
+    @Test
+    void passwordContainNoDiget() {
+        Assertions.assertEquals(false, passwordValidator.containsDiget("testABC"));
+    }
     }
